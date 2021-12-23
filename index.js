e2c9102443e5a1c1159603a8826d9a15680dc8b0
Joseph <jsltran@gmail.com>
2021-12-22 19:06:31 -0800

    commit tree 21a328851dfa49d7791519ab2db3744f9e5a07ae

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function (twoDrivers) {
+    let firstTwoDrivers = twoDrivers.slice(0,2);
+    return firstTwoDrivers;
+}
+
+const returnLastTwoDrivers = function (lastTwo) {
+    let lastTwoDrivers = lastTwo.slice(-2);
+    return lastTwoDrivers; 
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+
+    let fareMultiplier = function(fare) {
+      return fare * multiplier;
+    }
+    return fareMultiplier; 
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+const selectDifferentDrivers = function(allDrivers, driverSelector) {
+    return driverSelector(allDrivers);
+  }
+  
